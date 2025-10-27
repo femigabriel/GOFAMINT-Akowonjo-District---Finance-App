ec920593a46c0f3c8c2ec8192d6b52498aeafa59
Femi Gabriel <timfemi@gmail.com>
2025-10-27 15:21:37 +0100

    fixes

--- components/admin/dashbord/UsersContent.tsx
+++ components/admin/dashbord/UsersContent.tsx
@@ -1,7 +1,7 @@
 // components/admin/UsersContent.tsx
 "use client";
 
-import { Card, Table, Button, Tag, Space, Input, message, Avatar, Switch, Select } from "antd";
+import { Card, Table, Button, Tag, Space, Input, message, Avatar, Switch, Select, Option } from "antd";
 import { Users, Plus, Search, Edit, Trash2, Mail, Phone, UserCheck } from "lucide-react";
 
 const { Search: SearchInput } = Input;
